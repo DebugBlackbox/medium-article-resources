48b6c87a1e71346e15f2abb151712dd66d800ba9
Ankit Kumar <ankit.imaddi47@gmail.com>
2026-01-18 06:28:52 +0530

    feat: add user-add API toi insert users

--- src/routes/users.js
+++ src/routes/users.js
@@ -12,4 +12,13 @@ router.get('/all', (req, res) => {
     });
 });
 
+router.put('/add', (req, res) => {
+    const { username, email } = req.body;
+    global.db('users').insert({ username, email, password_hash: 'dummy_hash' }).then(() => {
+        res.status(201).send('User added successfully');
+    }).catch(err => {
+        res.status(500).send('Error adding user');
+    });
+});
+
 export default router;
