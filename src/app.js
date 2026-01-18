4c1eb7194706afab77f49439ce2a5d00cb368c1a
Ankit Kumar <ankit.imaddi47@gmail.com>
2026-01-18 06:28:13 +0530

    feat: add server to support POST type request handle capability

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express from 'express';
 import userRoutes from './routes/users.js';
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('ImmaJson is here!');
 });
